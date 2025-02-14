00978e7280c4d6c79aff2098ecf278d44d92fdcb
kino <kinogtag@outlook.com>
2025-02-14 22:56:30 +0000

    Release Branch
    Here's the official release of MiniInModded!
    
    What the mod does:
    - Upon joining a modded lobby, you will become small!
    
    Issues:
    - There is no way to disable it ingame apart from leaving the lobby. I will work on that.
    
    For now, enjoy the official release!
    
    (this took 3 weeks to make)

---
+++ Plugin.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using BepInEx;
+using GorillaLocomotion;
+using MiniMod;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilla;
+using Utilla.Attributes;
+
+namespace MiniInModded
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin("com.kino.gorillatag.miniinmodded", "MiniInModded", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private GameObject TinySizerENT;
+        private bool inRoom;
+        private GameObject clonedObject;
+
+        private void Start()
+        {
+            Events.GameInitialized += new EventHandler(OnGameInitialized);
+        }
+
+        private void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        private void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        private void OnGameInitialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+            SceneManager.LoadScene("Basement", LoadSceneMode.Additive);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            GameObject.Find("TinySizerEntrance(Clone)").SetActive(true);
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "Basement" && NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+            {
+                    SceneManager.sceneLoaded -= OnSceneLoaded;
+
+                    TinySizerENT = GameObject.Find("Basement/DungeonRoomAnchor/DungeonBasement/MazeSizeChangers/TinySizerEntrance");
+                    if (TinySizerENT != null)
+                    {
+                        TinySizerENT.transform.localScale = new Vector3(9999f, 9999f, 9999f);
+                        if (clonedObject == null)
+                        {
+                            clonedObject = Instantiate(TinySizerENT, new Vector3(0, 0, 0), Quaternion.identity);
+                        }
+                    }
+
+                    else
+                    {
+                        Debug.LogError("tse not found haha");
+                    }
+                    GameObject basementMusic = GameObject.Find("Basement/DungeonRoomAnchor/DungeonBasement/BasementMusicSpeaker");
+                    if (basementMusic != null)
+                    {
+                        basementMusic.SetActive(false);
+                    }
+                    RemoveAnnoyingAmbience();
+            }
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            SetPlayerPosition();
+            GameObject.Find("TinySizerEntrance(Clone)").SetActive(false);
+            inRoom = false;
+        }
+
+        private void SetPlayerPosition()
+        {
+            Player.Instance.transform.position = new Vector3(-68.5887f, 12.0883f, -83.958f);
+        }
+
+        private void RemoveAnnoyingAmbience()
+        {
+            GameObject crickets = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/AudioCrickets");
+            if (crickets != null)
+            {
+                crickets.SetActive(false);
+            }
+
+            GameObject rain = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/rain");
+            if (rain != null)
+            {
+                rain.SetActive(false);
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.kino.gorillatag.miniinmodded";
+        public const string Name = "MiniInModded";
+        public const string Version = "1.0.1";
+    }
+}
