17190cf31a1c1b60bbbabcb5f2abd90223eeb3d9
kino <kinogtag@outlook.com>
2025-04-03 20:16:17 +0100

    ahhh i forgot

--- ExamplePatch.cs
+++ ExamplePatch.cs
@@ -8,11 +8,11 @@ namespace MiniMod.Patches
     /// <summary>
     /// This is an example patch, made to demonstrate how to use Harmony. You should remove it if it is not used.
     /// </summary>
-    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch(typeof(GorillaLocomotion.GTPlayer))]
     [HarmonyPatch("Awake", MethodType.Normal)]
     internal class ExamplePatch
     {
-        private static void Postfix(GorillaLocomotion.Player __instance)
+        private static void Postfix(GorillaLocomotion.GTPlayer __instance)
         {
             Console.WriteLine(__instance.maxJumpSpeed);
         }
